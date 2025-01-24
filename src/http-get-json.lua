8d713fdf688eed7f049faefb6582681c0c941466
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-01-24 10:27:16 +0330

    feat: add http get request with json response

---
+++ src/http-get-json.lua
@@ -0,0 +1,30 @@
+apiUrl = "http://10.42.0.1/json.php"
+http.get(apiUrl, nil, function(code, data)
+    if (code < 0) then
+        print("HTTP request failed")
+    else
+        if code == 200 then
+            t = sjson.decode(data)
+            data = t.data
+
+            print('Command: ' .. t.command)
+            print('System: ' .. t.uname)
+            print('Time: ' .. t['time'])
+            print('Date & Time: ' .. t['date_time'])
+
+            if data == nil then
+                print('no data')
+                return
+            end
+            
+            if type(data) == 'table' and #data > 0 then
+                print('Data:')
+                for k, v in pairs(data) do
+                    print('ID: ' .. v.id .. ', Name: ' .. v.name)
+                end
+            else
+                print('No data found')
+            end
+        end
+    end
+end)
