70e7f63ba8e376a30b62061a5431f304f7febc99
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-01-30 20:26:04 +0330

    feat: add a binary release and alias to flash it

--- tools/aliases.sh
+++ tools/aliases.sh
@@ -1,3 +1,5 @@
 NODEMCU_PORT=/dev/ttyUSB0
+ESPTOOL=esptool.py
 alias nt-help='nodemcu-tool'
 alias nt="nodemcu-tool -p ${NODEMCU_PORT} "
+alias esp-flash="${ESPTOOL} -p ${NODEMCU_PORT} write_flash -fm qio 0x0 bin/nodemcu-release-20-modules-2025-01-03-21-00-16-float.bin"
