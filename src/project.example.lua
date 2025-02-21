c21dffb3bef774e9839c04913dd000aab98ab4ce
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-02-21 16:22:15 +0330

    feat: add project.example.lua file

---
+++ src/project.example.lua
@@ -0,0 +1,4 @@
+-------------------------------------------------------------------------------
+-- Variables in config.lua file can be overrided here. So the config.lua file 
+-- is actually a template for per-project override (using this file)
+print("! /project loaded")
