62422778a6881c242e46a40967461ed9cd0bf803
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-02-21 16:21:38 +0330

    feat: add more shell aliases

--- tools/aliases.sh
+++ tools/aliases.sh
@@ -3,3 +3,5 @@ ESPTOOL=esptool.py
 alias nt-help='nodemcu-tool'
 alias nt="nodemcu-tool -p ${NODEMCU_PORT} "
 alias esp-flash="${ESPTOOL} -p ${NODEMCU_PORT} write_flash -fm qio 0x0 bin/nodemcu-release-20-modules-2025-01-03-21-00-16-float.bin"
+alias ntput="nodemcu-tool -p ${NODEMCU_PORT} upload "
+alias ntget="nodemcu-tool -p ${NODEMCU_PORT} download "
